5fee482165994ca5d2038a4fd4deed5c893e7246
fujimoto kyosuke <kyoro.f@gmail.com>
2020-03-25 23:43:44 +0900

    Initial commit

---
+++ src/main/kotlin/App.kt
@@ -0,0 +1,73 @@
+package com.github.lusingander
+
+fun main() {
+    println(buildQuery())
+}
+
+fun buildQuery(): ObjectNode {
+    return repository {
+        name()
+        description()
+        owner {
+            login()
+        }
+        stargazers(first = 10) {
+            totalCount()
+            nodes {
+                name()
+            }
+        }
+    }
+}
+
+open class ObjectNode(val name: String) {
+    val children = mutableListOf<ObjectNode>()
+
+    fun <T : ObjectNode> doInit(child: T, init: T.() -> Unit = {}) {
+        child.init()
+        children.add(child)
+    }
+
+    override fun toString() =
+        "$name { ${children.joinToString(" ")} }"
+}
+
+open class ScalarNode(name: String): ObjectNode(name) {
+    override fun toString() = name
+}
+
+class REPOSITORY: ObjectNode("repository") {
+    fun name() = doInit(NAME())
+    fun description() = doInit(DESCRIPTION())
+    fun owner(init: OWNER.() -> Unit) =
+        doInit(OWNER(), init)
+    fun stargazers(first: Int = 0, init: STARGAZERS.() -> Unit) =
+        doInit(STARGAZERS(first), init)
+}
+
+class NAME: ScalarNode("name")
+
+class DESCRIPTION: ScalarNode("description")
+
+class OWNER: ObjectNode("owner") {
+    fun login() = doInit(LOGIN())
+}
+
+class LOGIN: ScalarNode("login")
+
+class STARGAZERS(private val first: Int): ObjectNode("stargazers") {
+    fun totalCount() = doInit(TOTAL_COUNT())
+    fun nodes(init: STARGAZERS_NODES.() -> Unit) =
+        doInit(STARGAZERS_NODES(), init)
+
+    override fun toString() =
+        "$name(first: $first) { ${children.joinToString(" ")} }"
+}
+
+class TOTAL_COUNT: ScalarNode("totalCount")
+
+class STARGAZERS_NODES: ObjectNode("nodes") {
+    fun name() = doInit(NAME())
+}
+
+fun repository(init: REPOSITORY.() -> Unit) = REPOSITORY().apply(init)
